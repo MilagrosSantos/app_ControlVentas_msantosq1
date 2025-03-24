0f992343ad9bf426befe3bb97140b6cc949ff439
Milagros <milagritospilar2401@gmail.com>
2025-03-24 15:23:58 -0500

    ProyectoFinal

---
+++ app_ControlVentas_msantosq/src/main/java/com/mycompany/app_controlventas_msantosq/view/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.app_controlventas_msantosq.view;
+
+/**
+ *
+ * @author Milagritos Quintana
+ */
+public class Main {
+     public static void main(String[] args) {
+        new SplashScreen(); 
+    }
+
+}
